cb75e82a4bd865825f234876eab7e96349f7b64d
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-11 16:32:53 +0300

    add

--- 01_Problems_01_to_13/04.cpp
+++ 01_Problems_01_to_13/04.cpp
@@ -3,3 +3,15 @@
 Number Of Days-Hours-Minutes-Seconds in Years
 
 */
+#include<iostream>
+using namespace std;
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
