c6723b5d70145ac2052ed8ae2a8b415032d0d2d6
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-11 17:13:11 +0300

    add

--- 01_Problems_01_to_13/07.cpp
+++ 01_Problems_01_to_13/07.cpp
@@ -3,3 +3,53 @@
 Mount Calendar 
 
 */
+#include <iostream> 
+using namespace std;  
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\n\nPlease Enter A Month Number: ";
+    cin >> Month;
+
+    return Month;
+}
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+short NumberOfDaysInMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+int main()
+{
+    short Year = ReadYear();
+    short Month = ReadMonth();
+
+    cout << "\n\n";
+}
