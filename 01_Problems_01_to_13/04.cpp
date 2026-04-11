5a1de0af38f0ffd670dc3401fdeddddfcefb4630
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-11 16:37:21 +0300

    add

--- 01_Problems_01_to_13/04.cpp
+++ 01_Problems_01_to_13/04.cpp
@@ -27,14 +27,63 @@ bool IsLeapYear(short Year)
         return false;
     }
 }
-int NumberOfDays(short Year)
+short NumberOfDays(short Year)
 {
-    
+    if (IsLeapYear)
+    {
+        return Year / 366;
+    }
+    else
+    {
+        return Year / 365;
+    }
 }
+
+short NumberOfHours(short Year)
+{
+    if (IsLeapYear)
+    {
+        return NumberOfDays(Year) * 24;
+    }
+    else
+    {
+        return NumberOfDays(Year) * 24;
+    }
+}
+
+short NumberOfMinutes(short Year)
+{
+    if (IsLeapYear)
+    {
+        return NumberOfHours(Year) * 60;
+    }
+    else
+    {
+        return NumberOfHours(Year) * 60;
+    }
+}
+
+short NumberOfSeconds(short Year)
+{
+    if (IsLeapYear)
+    {
+        return NumberOfMinutes(Year) * 60;
+    }
+    else
+    {
+        return NumberOfMinutes(Year) * 60;
+    }
+}
+
 
 int main()
 {
     short Year = ReadYear();
+    cout << "Number Of Days In " << Year << " Is: " << NumberOfDays(Year) << endl;
+    cout << "Number Of Hours In " << Year << " Is: " << NumberOfHours(Year) << endl;
+    cout << "Number Of Minutes In " << Year << " Is: " << NumberOfMinutes(Year) << endl;
+    cout << "Number Of Seconds In " << Year << " Is: " << NumberOfSeconds(Year) << endl;
+    
 
     return 0;
 }
