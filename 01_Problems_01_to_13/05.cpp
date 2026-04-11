b8afb04b3e73a7e598547c1703d31e88292f8ee4
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-11 16:44:34 +0300

    add

--- 01_Problems_01_to_13/05.cpp
+++ 01_Problems_01_to_13/05.cpp
@@ -3,3 +3,25 @@
 Number Of Days-Hours-Minutes-Seconds in Months
 
 */
+#include <iostream>
+using namespace std;
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "Please Enter A Month Number: ";
+    cin >> Month;
+
+    return Month;
+}
