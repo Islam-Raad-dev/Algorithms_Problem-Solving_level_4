e7fd613ec3a1c69019a3705dbc30b44ff118aa2a
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-11 16:50:52 +0300

    add

--- 01_Problems_01_to_13/05.cpp
+++ 01_Problems_01_to_13/05.cpp
@@ -20,8 +20,69 @@ short ReadMonth()
 {
     short Month;
 
-    cout << "Please Enter A Month Number: ";
+    cout << "\n\nPlease Enter A Month Number: ";
     cin >> Month;
 
     return Month;
+}
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+short NumberOfDaysinMonth(short Year, short Month)
+{
+    switch (Month)
+    {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        return 31;
+
+    case 4: case 6: case 9: case 11:
+        return 30;
+        
+    case 2:
+        return IsLeapYear(Year) ? 29 : 28;
+    default:
+        return 0;
+    }
+}
+
+short NumberOfHoursinMonth(short Year, short Month)
+{
+    return NumberOfDaysinMonth(Year, Month) * 24;
+}
+
+int NumberOfMinutesinMonth(short Year, short Month)
+{
+    return NumberOfHoursinMonth(Year, Month) * 60;
+}
+
+int NumberOfSecondsinMonth(short Year, short Month)
+{
+    return NumberOfMinutesinMonth(Year, Month) * 60;
+}
+
+int main()
+{
+    short Year = ReadYear();
+    short Month = ReadMonth();
+
+    cout << "\n\n";
+
+    cout << "Number Of Days In " << Month << " Is: " << NumberOfDaysinMonth(Year, Month) << endl;
+
+    cout << "Number Of Hours In " << Month << " Is: " << NumberOfHoursinMonth(Year, Month) << endl;
+
+    cout << "Number Of Minutes In " << Month << " Is: " << NumberOfMinutesinMonth(Year, Month) << endl;
+
+    cout << "Number Of Seconds In " << Month << " Is: " << NumberOfSecondsinMonth(Year, Month) << endl;
+
+
+    return 0;
 }
