718ce6d29c2d044a52bbb20c58d7b10d9d7be31c
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-11 16:30:30 +0300

    Leap Year (Short Version)

---
+++ 01_Problems_01_to_13/03.cpp
@@ -0,0 +1,47 @@
+/*
+
+Leap Year (Short Version)
+
+
+*/
+#include <iostream>
+using namespace std;
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+int main()
+{
+    short Year = ReadYear();
+
+    if (IsLeapYear(Year))
+    {
+        cout << Year << " Is A Leap Year.\n";
+    }
+    else
+    {
+        cout << Year << " Is Not A Leap Year.\n";
+    }
+
+    return 0;
+}
