308ef34c6fa6da5dc63322a57598c3ac5b0a24b8
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-12 12:03:40 +0300

    add

---
+++ 01_Problems_01_to_13/10.cpp
@@ -0,0 +1,37 @@
+/*
+
+Days From The Beginning of the Year
+
+*/
+#include <iostream>
+using namespace std;
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\n\nPlease Enter A Month Number: ";
+    cin >> Month;
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day;
+
+    cout << "\n\nPlease Enter A Day Number: ";
+    cin >> Day;
+
+    return Day;
+}
