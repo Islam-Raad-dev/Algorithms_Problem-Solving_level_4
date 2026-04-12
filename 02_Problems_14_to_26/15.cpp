1d2cbabc2f4fa6b4935759ce54b8dbb65ed102bf
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-12 19:20:25 +0300

    add

--- 02_Problems_14_to_26/15.cpp
+++ 02_Problems_14_to_26/15.cpp
@@ -64,6 +64,18 @@ sDate ReadFullDate()
     return Date;
 }
 
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 short NumberOfDaysInMonth(short Year, short Month)
 {
     if (Month < 1 || Month > 12)
