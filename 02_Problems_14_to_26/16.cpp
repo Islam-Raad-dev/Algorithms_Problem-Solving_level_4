1ead318ba7fc530c4c6a9090de7c6a03aed06049
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-12 19:31:27 +0300

    Add

--- 02_Problems_14_to_26/16.cpp
+++ 02_Problems_14_to_26/16.cpp
@@ -3,3 +3,121 @@
 Increase a Date by One Day
 
 */
+
+#include <iostream>
+using namespace std;
+
+struct sDate
+{
+    short Day;
+    short Month;
+    short Year;
+};
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\n\nPlease Enter A Month: ";
+    cin >> Month;
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day;
+
+    cout << "\n\nPlease Enter A Day: ";
+    cin >> Day;
+
+    return Day;
+}
+
+short ReadDayToAdd()
+{
+    short Days;
+
+    cout << "\n\nPlease Enter A Days To Add: ";
+    cin >> Days;
+
+    return Days;
+}
+
+sDate ReadFullDate()
+{
+    sDate Date;
+
+    Date.Year = ReadYear();
+    Date.Month = ReadMonth();
+    Date.Day = ReadDay();
+
+    return Date;
+}
+
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+short NumberOfDaysInMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+sDate IncreaseDateByOneDay(sDate Date)
+{
+    if (Date.Day == NumberOfDaysInMonth(Date.Year, Date.Month))
+    {
+        if (Date.Month == 12)
+        {
+            Date.Month = 1;
+            Date.Day = 1;
+            Date.Year++;
+        }
+        else
+        {
+            Date.Month++;
+            Date.Day = 1;
+        }
+    }
+    else
+    {
+        Date.Day++;
+    }
+    return Date;
+}
+
+int main()
+{
+    sDate Date = ReadFullDate();
+
+    Date = IncreaseDateByOneDay(Date);
+
+    cout << "Date After Adding One Day Is: " << Date.Day << "/" << Date.Month << "/" << Date.Year << "\n"
+         << endl;
+
+    return 0;
+}
