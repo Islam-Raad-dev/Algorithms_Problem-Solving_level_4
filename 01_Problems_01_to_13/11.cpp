fdc46836c7c06344183c2542a29f9c717cfab366
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-12 12:11:45 +0300

    add

--- 01_Problems_01_to_13/11.cpp
+++ 01_Problems_01_to_13/11.cpp
@@ -3,3 +3,83 @@
 Date From Day Order In A Year
 
 */
+
+#include <iostream>
+using namespace std;
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\n\nPlease Enter A Month: ";
+    cin >> Month;
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day;
+
+    cout << "\n\nPlease Enter A Day: ";
+    cin >> Day;
+
+    return Day;
+}
+
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+short NumberOfDaysInMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+short DaysFromBeginningOfYear(short Year, short Month, short Day)
+{
+    short TotalDays = 0;
+
+    for (short i = 1; i <= Month - 1; i++)
+    {
+        TotalDays += NumberOfDaysInMonth(Year, i);
+    }
+
+    TotalDays += Day;
+
+    return TotalDays;
+}
+
+int main()
+{
+    short Year = ReadYear();
+    short Month = ReadMonth();
+    short Day = ReadDay();
+
+    cout << "\n\nNumber of days from the beginning of the year: " << DaysFromBeginningOfYear(Year, Month, Day) << endl;
+
+    return 0;
+}
