7d3b0f810a893dec055b3b41775938ed51f65b28
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-12 19:38:11 +0300

    add

--- 02_Problems_14_to_26/17.cpp
+++ 02_Problems_14_to_26/17.cpp
@@ -3,3 +3,93 @@
 Diff In Days
 
 */
+
+#include <iostream>
+using namespace std;
+
+struct sDate
+{
+    short Day;
+    short Month;
+    short Year;
+};
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "\nPlease Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\nPlease Enter A Month: ";
+    cin >> Month;
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day;
+
+    cout << "\nPlease Enter A Day: ";
+    cin >> Day;
+
+    return Day;
+}
+
+short ReadDayToAdd()
+{
+    short Days;
+
+    cout << "\n\nPlease Enter A Days To Add: ";
+    cin >> Days;
+
+    return Days;
+}
+
+sDate ReadFullDate()
+{
+    sDate Date;
+
+    Date.Year = ReadYear();
+    Date.Month = ReadMonth();
+    Date.Day = ReadDay();
+
+    return Date;
+}
+
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+        return true;
+    else
+        return false;
+}
+
+short NumberOfDaysInMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+bool IsLastDayInMonth(sDate Date1)
+{
+    return (Date1.Day == NumberOfDaysInMonth(Date1.Year, Date1.Month));
+}
+
+bool IsLastMonthInYear(sDate Date1)
+{
+    return (Date1.Month == 12);
+}
+
