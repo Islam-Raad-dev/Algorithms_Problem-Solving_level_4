ab1d26ff538880ee67c411889687a25698ca16ab
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-12 19:16:05 +0300

    Date1 Equals To  Date2

--- 02_Problems_14_to_26/14.cpp
+++ 02_Problems_14_to_26/14.cpp
@@ -3,3 +3,88 @@
 Date1 Equals To  Date2
 
 */
+#include <iostream>
+using namespace std;
+
+struct sDate
+{
+    short Day;
+    short Month;
+    short Year;
+};
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "\nPlease Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\nPlease Enter A Month: ";
+    cin >> Month;
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day;
+
+    cout << "\nPlease Enter A Day: ";
+    cin >> Day;
+
+    return Day;
+}
+
+short ReadDayToAdd()
+{
+    short Days;
+
+    cout << "\n\nPlease Enter A Days To Add: ";
+    cin >> Days;
+
+    return Days;
+}
+ 
+sDate ReadFullDate()
+{
+    sDate Date;
+
+    Date.Year = ReadYear();
+    Date.Month = ReadMonth();
+    Date.Day = ReadDay();
+
+    return Date;
+}
+
+bool IsDate1EqualDate2(sDate Date1, sDate Date2)
+{
+    
+    return (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day == Date2.Day) ? true : false;
+
+}
+
+int main()
+{
+    sDate Date1 = ReadFullDate();
+    sDate Date2 = ReadFullDate();
+
+    if (IsDate1EqualDate2(Date1, Date2))
+    {
+        cout << "\nYes, Date1 Is Equal To Date2.";
+    }
+
+    else
+    {
+        cout << "\nNo, Date1 Is Not Equal To Date2.";
+    }
+
+    return 0;
+}
