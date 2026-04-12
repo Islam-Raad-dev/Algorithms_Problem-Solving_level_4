caeddb0d3c48af5a5564026fd211aff2b90ee914
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-12 12:05:22 +0300

    Add

--- 01_Problems_01_to_13/10.cpp
+++ 01_Problems_01_to_13/10.cpp
@@ -35,3 +35,50 @@ short ReadDay()
 
     return Day;
 }
+
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+short NumberOfDaysInMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+short DaysFromBeginningOfYear(short Year, short Month, short Day)
+{
+    short TotalDays = 0;
+
+    for (short m = 1; m < Month; ++m)
+    {
+        TotalDays += NumberOfDaysInMonth(Year, m);
+    }
+
+    TotalDays += Day;
+
+    return TotalDays;
+}
+
+int main()
+{
+    short Year = ReadYear();
+    short Month = ReadMonth();
+    short Day = ReadDay();
+
+    cout << "\n\nNumber of days from the beginning of the year: " << DaysFromBeginningOfYear(Year, Month, Day) << endl;
+
+    return 0;
+}
