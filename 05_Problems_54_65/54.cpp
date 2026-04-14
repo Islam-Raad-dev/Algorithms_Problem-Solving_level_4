16c100d8d19db4a97eb78dbe1e7aa918ebe897a5
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-14 21:37:41 +0300

    Add

--- 05_Problems_54_65/54.cpp
+++ 05_Problems_54_65/54.cpp
@@ -3,3 +3,80 @@
 Caluclate Vaction Days
 
 */
+#include <iostream>
+using namespace std;
+
+struct sDate
+{
+    short Day;
+    short Month;
+    short Year;
+};
+    
+short ReadYear()
+{
+    short Year;
+
+    cout << "Please Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+} 
+     
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\n\nPlease Enter A Month: ";
+    cin >> Month;
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day;
+
+    cout << "\n\nPlease Enter A Day: ";
+    cin >> Day;
+
+    return Day;
+}
+
+sDate ReadFullDate()
+{
+    sDate Date;
+
+    Date.Year = ReadYear();
+    Date.Month = ReadMonth();
+    Date.Day = ReadDay();
+
+    return Date;
+}   
+      
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+short NumberOfDaysInMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];  
+}
+
+bool IsLastDayInMonth(sDate Date1)
+{
+    return (Date1.Day == NumberOfDaysInMonth(Date1.Year, Date1.Month));  
+}
