d3b43e30bae9e7036abe8477d0f83c28537b377f
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-14 22:32:26 +0300

    Add

--- 05_Problems_54_65/56.cpp
+++ 05_Problems_54_65/56.cpp
@@ -55,37 +55,18 @@ sDate ReadFullDate()
     return Date;
 }   
       
-bool IsLeapYear(short Year)
-{
-    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-short NumberOfDaysInMonth(short Year, short Month)
-{
-    if (Month < 1 || Month > 12)
-        return 0;
-
-    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
-}
-
-bool IsDate1LessThanDate2(sDate Date1, sDate Date2)
+bool IsDate1BeforeDate2(sDate Date1, sDate Date2)
 {
     return (Date1.Year < Date2.Year) ? true : (Date1.Year == Date2.Year && Date1.Month < Date2.Month) ? true : (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day < Date2.Day) ? true : false;
 }
 
+bool IsDate1EqualDate2(sDate Date1, sDate Date2)
+{
+    return (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day == Date2.Day) ? true : false;
+}
 bool IsDate1AfterDate2(sDate Date1, sDate Date2)
 {
-    return (Date1.Year > Date2.Year) ? true : (Date1.Year == Date2.Year && Date1.Month > Date2.Month) ? true : (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day > Date2.Day) ? true : false;
+    return (!IsDate1BeforeDate2(Date1, Date2) && !IsDate1EqualDate2(Date1, Date2)) ? true : false;
 }
 
 int main(){
@@ -98,11 +79,11 @@ int main(){
 
     if(IsDate1AfterDate2(Date1, Date2))
     {
-        cout << "\nYes, Date 1 is after Date 2.";
+        cout << "\nYes, Date 1 is after Date 2." << endl;
     }
     else
     {
-        cout << "\nNo, Date 1 is not after Date 2.";
+        cout << "\nNo, Date 1 is not after Date 2." << endl;
     }
 
     return 0;
