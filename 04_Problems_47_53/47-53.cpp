7b03fa51cdf2848c59659d5d0462082b7affc1f2
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-14 21:03:11 +0300

    Add

--- 04_Problems_47_53/47-53.cpp
+++ 04_Problems_47_53/47-53.cpp
@@ -12,3 +12,68 @@ struct sDate
     short Month;
     short Year;
 };
+
+sDate TodayIs()
+{
+}
+
+bool IsEndOfWeek(sDate Date)
+{
+}
+
+bool IsItWeekEnd(sDate Date)
+{
+}
+
+bool IsItBusinessDay(sDate Date)
+{
+}
+sDate DaysUntilTheEndOfTheWeek()
+{
+}
+
+sDate DaysUntilTheEndOfTheMonth()
+{
+}
+
+sDate DaysUntilTheEndOfTheYear()
+{
+}
+
+string DayShortName(sDate Date)
+{
+    string Days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+    return Days[Date.Day % 7];
+}
+sDate GetSystemDate()
+{
+}
+int main()
+{
+    sDate Date = GetSystemDate();
+
+    Date = TodayIs();
+
+    cout << "\nIs It End Of Week: \n";
+    if (IsItWeekEnd(Date))
+        cout << "Yes, It's Weekend\n";
+    else
+        cout << "No, It's Not Weekend\n";
+
+    cout << "\n\nIs It Weekend: \n";
+    if (IsItWeekEnd(Date))
+        cout << "Yes, It's Weekend\n";
+    else
+        cout << "No, It's Not Weekend\n";
+
+    cout << "\n\nIs It Business Day: \n";
+    if (IsItBusinessDay(Date))
+        cout << "Yes, It's Business Day\n";
+    else
+        cout << "No, It's Not Business Day\n";
+
+    cout << "\n\n;
+
+        return 0;
+}
