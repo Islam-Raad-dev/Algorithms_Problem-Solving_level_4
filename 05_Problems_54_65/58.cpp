01169dced449ecab274a47d8fcdfe8d02cd6b828
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-15 11:26:15 +0300

    Add

--- 05_Problems_54_65/58.cpp
+++ 05_Problems_54_65/58.cpp
@@ -28,8 +28,8 @@ short ReadYear()
     cin >> Year;
 
     return Year;
-}
-
+} 
+     
 short ReadMonth()
 {
     short Month;
@@ -74,15 +74,9 @@ Period2 ReadFullDate2()
 
 bool IsPeriodOverlap(Period1 StartPeriod1, Period1 EndPeriod1, Period2 StartPeriod2, Period2 EndPeriod2)
 {
-    return (StartPeriod1.Year < EndPeriod2.Year) ? true : (StartPeriod1.Year == EndPeriod2.Year && StartPeriod1.Month < EndPeriod2.Month)                                      ? true
-                                                      : (StartPeriod1.Year == EndPeriod2.Year && StartPeriod1.Month == EndPeriod2.Month && StartPeriod1.Day <= EndPeriod2.Day) ? true
-                                                      : (EndPeriod1.Year > StartPeriod2.Year)                                                                                  ? true
-                                                      : (EndPeriod1.Year == StartPeriod2.Year && EndPeriod1.Month > StartPeriod2.Month)                                        ? true
-                                                      : (EndPeriod1.Year == StartPeriod2.Year && EndPeriod1.Month == StartPeriod2.Month && EndPeriod1.Day >= StartPeriod2.Day) ? true
-                                                                                                                                                                               : false;
+    return (StartPeriod1.Year < EndPeriod2.Year) ? true : (StartPeriod1.Year == EndPeriod2.Year && StartPeriod1.Month < EndPeriod2.Month) ? true : (StartPeriod1.Year == EndPeriod2.Year && StartPeriod1.Month == EndPeriod2.Month && StartPeriod1.Day <= EndPeriod2.Day) ? true : (EndPeriod1.Year > StartPeriod2.Year) ? true : (EndPeriod1.Year == StartPeriod2.Year && EndPeriod1.Month > StartPeriod2.Month) ? true : (EndPeriod1.Year == StartPeriod2.Year && EndPeriod1.Month == StartPeriod2.Month && EndPeriod1.Day >= StartPeriod2.Day) ? true : false;
 }
-int main()
-{
+int main(){
 
     cout << "Please Enter First Period: " << endl;
     Period1 StartPeriod1 = ReadFullDate1();
@@ -92,13 +86,13 @@ int main()
     Period2 StartPeriod2 = ReadFullDate2();
     Period2 EndPeriod2 = ReadFullDate2();
 
-    if (IsPeriodOverlap(StartPeriod1, EndPeriod1, StartPeriod2, EndPeriod2))
+    if(IsPeriodOverlap(StartPeriod1, EndPeriod1, StartPeriod2, EndPeriod2))
     {
-        cout << "Yes, The Two Periods Overlap Each Other." << endl;
+        cout<< "Yes, The Two Periods Overlap Each Other." << endl;
     }
     else
     {
-        cout << "No, The Two Periods Do Not Overlap Each Other." << endl;
+        cout<< "No, The Two Periods Do Not Overlap Each Other." << endl;
     }
 
     return 0;
