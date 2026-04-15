741e748f2bd13cf3ada59cafed552eb725095db1
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-15 11:40:46 +0300

    Add

--- 05_Problems_54_65/58.cpp
+++ 05_Problems_54_65/58.cpp
@@ -73,6 +73,21 @@ sPeriod ReadFullPeriod()
     return Period;
 }
 
+bool IsDate1BeforeDate2(sDate Date1, sDate Date2)
+{
+    return (Date1.Year < Date2.Year) ? true : (Date1.Year == Date2.Year && Date1.Month < Date2.Month) ? true : (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day < Date2.Day) ? true : false;
+}
+
+bool IsDate1EqualDate2(sDate Date1, sDate Date2)
+{
+    return (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day == Date2.Day) ? true : false;
+}
+
+bool IsDate1AfterDate2(sDate Date1, sDate Date2)
+{
+    return (!IsDate1BeforeDate2(Date1, Date2) && !IsDate1EqualDate2(Date1, Date2)) ? true : false;
+}
+
 bool IsOverlap(sPeriod Period1, sPeriod Period2)
 {
     return (IsDate1BeforeDate2(Period1.StartDate, Period2.EndDate) && IsDate1AfterDate2(Period1.EndDate, Period2.StartDate)) || (IsDate1EqualDate2(Period1.StartDate, Period2.EndDate) || IsDate1EqualDate2(Period1.EndDate, Period2.StartDate));
