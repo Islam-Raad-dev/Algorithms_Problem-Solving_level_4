f448b1130d857e0219981ee1bbd36a1e916c1b66
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-16 16:33:43 +0300

    Add

--- 05_Problems_54_65/62.cpp
+++ 05_Problems_54_65/62.cpp
@@ -12,3 +12,44 @@ struct sDate
     short Month;
     short Year;
 };
+
+short ReadYear()
+{
+    short Year;
+
+    cout << "\nPlease Enter A Year: ";
+    cin >> Year;
+
+    return Year;
+}
+
+short ReadMonth()
+{
+    short Month;
+
+    cout << "\nPlease Enter A Month: ";
+    cin >> Month;
+
+    return Month;
+}
+
+short ReadDay()
+{
+    short Day;
+
+    cout << "\nPlease Enter A Day: ";
+    cin >> Day;
+
+    return Day;
+}
+
+sDate ReadFullDate()
+{
+    sDate Date;
+
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
