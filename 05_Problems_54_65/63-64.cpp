2b5b8767ac735697e24f9386d07605e46f73bf2a
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-16 16:42:57 +0300

    Add

--- 05_Problems_54_65/63-64.cpp
+++ 05_Problems_54_65/63-64.cpp
@@ -22,3 +22,32 @@ string ReadDateString()
 
     return DateString;
 }
+
+sDate ConvertStringToDate(string DateString)
+{
+    sDate Date;
+
+    Date.Day = stoi(DateString.substr(0, 2));
+    Date.Month = stoi(DateString.substr(3, 2));
+    Date.Year = stoi(DateString.substr(6, 4));
+
+    return Date;
+}
+
+void PrintStringDate(sDate Date)
+{
+    cout << "\nThe Date Is: " << Date.Day << "/" << Date.Month << "/" << Date.Year << endl;
+}
+
+int main()
+{
+    string DateString = ReadDateString();
+
+    sDate Date = ConvertStringToDate(DateString);
+
+    cout << "\nThe Date Is: " << Date.Day << "/" << Date.Month << "/" << Date.Year << endl;
+
+    PrintStringDate(Date);
+
+    return 0;
+}
