181fb35be0a687ad5b7f8c7549135bdb9cf34ca0
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-16 16:34:59 +0300

    Add

--- 05_Problems_54_65/62.cpp
+++ 05_Problems_54_65/62.cpp
@@ -53,3 +53,51 @@ sDate ReadFullDate()
 
     return Date;
 }
+
+bool IsLeapYear(short Year)
+{
+    if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+short NumberOfDaysInMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+bool IsItValidDate(sDate Date)
+{
+    if (Date.Year < 1 || Date.Month < 1 || Date.Month > 12 || Date.Day < 1 || Date.Day > NumberOfDaysInMonth(Date.Year, Date.Month))
+    {
+        return false;
+    }
+    return true;
+}
+int main()
+{
+
+    cout << "Please Enter A Date: " << endl;
+    sDate Date = ReadFullDate();
+
+    if (IsItValidDate(Date))
+    {
+        cout << "\nThe Date Is Valid." << endl;
+    }
+    else
+    {
+        cout << "\nThe Date Is Not Valid." << endl;
+    }
+
+
+    return 0;
+}
