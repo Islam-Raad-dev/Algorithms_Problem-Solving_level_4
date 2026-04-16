e7380ee0aa72d80310f1c7d73c218cc80cfc3573
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-16 16:53:21 +0300

    Add

--- 05_Problems_54_65/65.cpp
+++ 05_Problems_54_65/65.cpp
@@ -3,3 +3,71 @@
 Formate Date
 
 */
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct sDate
+{
+    short Day;
+    short Month;
+    short Year;
+};
+
+string ReadDateString(string Message)
+{
+    string DateString;
+
+    cout << Message;
+    getline(cin >> ws, DateString);
+
+    return DateString;
+}
+
+vector<string> SplitString(string str, string Delimiter)
+{
+    vector<string> vString;
+
+    string word = "";
+    for (char x : str)
+    {
+        if (x == Delimiter[0])  
+        {
+            vString.push_back(word);
+            word = "";
+        }
+        else
+        {
+            word = word + x;
+        }
+    }
+    vString.push_back(word);
+
+    return vString;
+}
+
+sDate StringToDateSturctur(string DateString)
+{
+    sDate Date;
+
+    vector <string> vDate;
+    
+    vDate = SplitString(DateString, "/");
+    Date.Day = stoi(vDate[0]);
+    Date.Month = stoi(vDate[1]);
+    Date.Year = stoi(vDate[2]);
+
+    return Date;
+}
+
+int main()
+{
+
+    string DateString = ReadDateString("Please Enter A Date (DD/MM/YYYY): ");
+    sDate Date = StringToDateSturctur(DateString);
+
+    cout << "\nThe Formated Date Is: " << Date.Day << "/" << Date.Month << "/" << Date.Year << endl;
+
+    return 0;
+}
