3a11565f9059302fb41d68d66de398531b5813ad
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-18 17:45:15 +0300

    aDD

--- Project's/Project_2.cpp
+++ Project's/Project_2.cpp
@@ -54,6 +54,51 @@ enum enQuickWithdrawal
     eQWExit = 9
 };
 
+vector<string> SplitString(string S1, string Delim)
+{
+    vector<string> vString;
+    short pos = 0;
+    string sWord;
+
+    while ((pos = S1.find(Delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0, pos);
+
+        if (sWord != "") vString.push_back(sWord);
+
+        S1.erase(0, pos + Delim.length());
+    }
+
+    if (S1 != "") vString.push_back(S1);
+
+    return vString;
+}
+
+// تحويل سطر نصي إلى سجل مستخدم
+string Trim(string s)
+{
+    static const string whitespaces = " \t\r\n\f\v";
+    size_t first = s.find_first_not_of(whitespaces);
+    if (string::npos == first) return "";
+    size_t last = s.find_last_not_of(whitespaces);
+    return s.substr(first, (last - first + 1));
+}
+
+sClient ConvertLinetoRecord(string Line, string Seperator = "#//#")
+{
+    sClient Client;
+    vector<string> vClientData;
+
+    vClientData = SplitString(Line, Seperator);
+
+    Client.AccountNumber = vClientData[0];
+    Client.PinCode = vClientData[1];
+    Client.Name = vClientData[2];
+    Client.Phone = vClientData[3];
+    Client.AccountBalance = stod(vClientData[4]);
+
+    return Client;
+}
 vector<sClient> LoadCleintsDataFromFile(string FileName)
 {
     vector<sClient> vClients;
